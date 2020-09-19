0e7482d435a15cb726882fa088e4c5d6ee60cea8
Breno Nunes <breno.arantes97@gmail.com>
2020-09-18 23:32:40 -0300

    Base project from Spring Tutorial

---
+++ src/main/java/io/github/brenovit/messagingstompwebsocket/payload/Greeting.java
@@ -0,0 +1,10 @@
+package io.github.brenovit.messagingstompwebsocket.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+	private String content;
+}
