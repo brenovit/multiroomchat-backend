9513600a90ce68b77cfdf376d740efb0502d6af4
Breno Nunes <breno.arantes97@gmail.com>
2020-09-18 23:32:40 -0300

    Base project from Spring Tutorial

---
+++ src/main/java/io/github/brenovit/messagingstompwebsocket/MultiroomchatStompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package io.github.brenovit.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiroomchatStompWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiroomchatStompWebsocketApplication.class, args);
+	}
+
+}
