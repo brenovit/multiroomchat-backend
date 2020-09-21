a56ef1670e78848b8829770b2a09c26659d76c2f
Breno Nunes <breno.arantes97@gmail.com>
2020-09-20 22:01:28 -0300

    Remove restTemplate

--- src/main/java/io/github/brenovit/messagingstompwebsocket/MultiroomchatStompWebsocketApplication.java
+++ src/main/java/io/github/brenovit/messagingstompwebsocket/MultiroomchatStompWebsocketApplication.java
@@ -2,9 +2,7 @@ package io.github.brenovit.messagingstompwebsocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.web.client.RestTemplate;
 
 @EnableMongoAuditing
 @SpringBootApplication
@@ -13,10 +11,4 @@ public class MultiroomchatStompWebsocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MultiroomchatStompWebsocketApplication.class, args);
 	}
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 }
